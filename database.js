4ca15d0c0c1a5b3ca789c0892d90796f97c7d74b
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2024-03-28 15:24:15 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "a05491ae9c15e46229bba0450d258dcc-700465491.us-west-2.elb.amazonaws.com",
+  host: "a31fb040e40ca40259e44f70e681e5cc-1148870372.us-east-2.elb.amazonaws.com",
   user: "root",
   password: "Root@123",
   database: 'pharmacy',
